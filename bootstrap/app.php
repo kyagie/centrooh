a7a85b37e5f1cae7576085c52c8f83b951ea24a2
Arthur Kirabo <kyagieboy99@gmail.com>
2025-05-09 14:22:19 +0300

    Remove EnsureUserIsAgent middleware from application configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Middleware\EnsureUserIsAgent;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -14,7 +13,6 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
-        $middleware->append(EnsureUserIsAgent::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
