a0f8c1cc8df7e42a2c9ca941a9d8107975569e0a
Arthur Kirabo <kyagieboy99@gmail.com>
2025-05-13 23:29:56 +0300

    Update navigation icons in DeviceResource, OneTimePasswordResource, and RegionResource for consistency

--- app/Filament/Resources/DeviceResource.php
+++ app/Filament/Resources/DeviceResource.php
@@ -17,7 +17,7 @@ class DeviceResource extends Resource
 {
     protected static ?string $model = Device::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-phone';
 
     public static function form(Form $form): Form
     {
--- app/Filament/Resources/OneTimePasswordResource.php
+++ app/Filament/Resources/OneTimePasswordResource.php
@@ -17,7 +17,7 @@ class OneTimePasswordResource extends Resource
 {
     protected static ?string $model = OneTimePassword::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-key';
 
     public static function form(Form $form): Form
     {
