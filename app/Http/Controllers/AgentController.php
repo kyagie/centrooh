27403625cd17cbd250a1b8b51f7f728f611923d6
Arthur Kirabo <kyagieboy99@gmail.com>
2025-05-16 11:20:53 +0300

    Refactor AgentController to use BillboardImageService for image processing and remove redundant methods

--- app/Http/Controllers/AgentController.php
+++ app/Http/Controllers/AgentController.php
@@ -8,7 +8,7 @@ use App\Models\{
     Device,
     OneTimePassword,
     Billboard,
-    BillboardImage
+    AgentNotification,
 };
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -18,12 +18,17 @@ use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 use Illuminate\Http\File;
-use Intervention\Image\Laravel\Facades\Image;
-use Intervention\Image\Typography\FontFactory;
-use Intervention\Image\Geometry\Factories\RectangleFactory;
+use App\Services\BillboardImageService;
 
 class AgentController extends Controller
 {
+    protected BillboardImageService $billboardImageService;
+
+    public function __construct(BillboardImageService $billboardImageService)
+    {
+        $this->billboardImageService = $billboardImageService;
+    }
+
     /**
      * Register a new agent.
      *
@@ -228,8 +233,8 @@ class AgentController extends Controller
         }
 
         $image = $request->file('image');
-        $imageName = $this->generateImageName($billboard->name, $image->extension());
-        $formattedImageName = $this->addTextToImage($image, $billboard->toArray(), $imageName);
+        $imageName = $this->billboardImageService->generateImageName($billboard->name, $image->extension());
+        $formattedImageName = $this->billboardImageService->addTextToImage($image, $billboard->toArray(), $imageName);
         $imagePath = public_path("images/{$formattedImageName}");
 
         try {
@@ -259,62 +264,5 @@ class AgentController extends Controller
             'status' => 'success',
             'message' => 'Image uploaded successfully',
         ]);
-    }
-
-    /**
-     * Adds billboard metadata text to the image.
-     *
-     * @param  \Illuminate\Http\UploadedFile  $image
-     * @param  array  $billboardInfo
-     * @param  string  $imageName
-     * @return string
-     */
-    protected function addTextToImage($image, array $billboardInfo, string $imageName)
-    {
-        $img = Image::read($image);
-        $imgWidth = $img->width();
-        $imgHeight = $img->height();
-
-        $x = 0;
-        $y = $imgHeight - 160;
-        $currentDate = date('Y-m-d(D) H:i');
-
-        $text = <<<EOT
-Latitude: {$billboardInfo['latitude']}
-Longitude: {$billboardInfo['longitude']}
-{$currentDate}
-EOT;
-
-        $img->drawRectangle(
-            $x,
-            $y,
-            fn(RectangleFactory $rectangle) =>
-            $rectangle->size($imgWidth, 160)->background('#0000')
-        );
-
-        $img->text($text, $imgWidth / 2, $y + 60, function (FontFactory $font) {
-            $font->filename(public_path('assets/fonts/Exo2-Bold.otf'));
-            $font->size(38);
-            $font->color('#FFDB58');
-            $font->align('center');
-            $font->valign('center');
-        });
-
-        $img->save(public_path("images/{$imageName}"));
-
-        return $imageName;
-    }
-
-    /**
-     * Generate image file name with timestamp.
-     *
-     * @param  string  $name
-     * @param  string  $extension
-     * @return string
-     */
-    protected function generateImageName(string $name, string $extension): string
-    {
-        $timestamp = now()->format('Ymd_His');
-        return Str::snake($name) . "_{$timestamp}.{$extension}";
     }
 }
