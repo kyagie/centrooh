bec32c9e0219cb4d77ce5b7a24bf30c5d1a83b35
Arthur Kirabo <kyagieboy99@gmail.com>
2025-05-19 16:57:04 +0300

    Refactor canAccessPanel method: simplify role check by removing unnecessary user() call.

--- app/Models/User.php
+++ app/Models/User.php
@@ -135,7 +135,7 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with($this->email, '@centrooh.app') && $this->user()->hasRole([
+        return str_ends_with($this->email, '@centrooh.app') && $this->hasRole([
             'admin',
             'super-admin',
         ]);
