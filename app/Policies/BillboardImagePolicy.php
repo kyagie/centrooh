c7d10e0fe8f85cd64eeb2486ad55b144b00b7016
Arthur <kyagieboy99@gmail.com>
2025-05-23 16:26:22 +0000

    Permission Changes

--- app/Policies/BillboardImagePolicy.php
+++ app/Policies/BillboardImagePolicy.php
@@ -3,10 +3,10 @@
 namespace App\Policies;
 
 use App\Models\User;
-use App\Models\AgentNotificationType;
+use App\Models\BillboardImage;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
-class AgentNotificationTypePolicy
+class BillboardImagePolicy
 {
     use HandlesAuthorization;
 
@@ -15,15 +15,15 @@ class AgentNotificationTypePolicy
      */
     public function viewAny(User $user): bool
     {
-        return $user->can('view_any_agent::notification::type');
+        return $user->can('view_any_billboard::image');
     }
 
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, AgentNotificationType $agentNotificationType): bool
+    public function view(User $user, BillboardImage $billboardImage): bool
     {
-        return $user->can('view_agent::notification::type');
+        return $user->can('view_billboard::image');
     }
 
     /**
@@ -31,23 +31,23 @@ class AgentNotificationTypePolicy
      */
     public function create(User $user): bool
     {
-        return $user->can('create_agent::notification::type');
+        return $user->can('create_billboard::image');
     }
 
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, AgentNotificationType $agentNotificationType): bool
+    public function update(User $user, BillboardImage $billboardImage): bool
     {
-        return $user->can('update_agent::notification::type');
+        return $user->can('update_billboard::image');
     }
 
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, AgentNotificationType $agentNotificationType): bool
+    public function delete(User $user, BillboardImage $billboardImage): bool
     {
-        return $user->can('delete_agent::notification::type');
+        return $user->can('delete_billboard::image');
     }
 
     /**
@@ -55,15 +55,15 @@ class AgentNotificationTypePolicy
      */
     public function deleteAny(User $user): bool
     {
-        return $user->can('delete_any_agent::notification::type');
+        return $user->can('delete_any_billboard::image');
     }
 
     /**
      * Determine whether the user can permanently delete.
      */
-    public function forceDelete(User $user, AgentNotificationType $agentNotificationType): bool
+    public function forceDelete(User $user, BillboardImage $billboardImage): bool
     {
-        return $user->can('force_delete_agent::notification::type');
+        return $user->can('force_delete_billboard::image');
     }
 
     /**
@@ -71,15 +71,15 @@ class AgentNotificationTypePolicy
      */
     public function forceDeleteAny(User $user): bool
     {
-        return $user->can('force_delete_any_agent::notification::type');
+        return $user->can('force_delete_any_billboard::image');
     }
 
     /**
      * Determine whether the user can restore.
      */
-    public function restore(User $user, AgentNotificationType $agentNotificationType): bool
+    public function restore(User $user, BillboardImage $billboardImage): bool
     {
-        return $user->can('restore_agent::notification::type');
+        return $user->can('restore_billboard::image');
     }
 
     /**
@@ -87,15 +87,15 @@ class AgentNotificationTypePolicy
      */
     public function restoreAny(User $user): bool
     {
-        return $user->can('restore_any_agent::notification::type');
+        return $user->can('restore_any_billboard::image');
     }
 
     /**
      * Determine whether the user can replicate.
      */
-    public function replicate(User $user, AgentNotificationType $agentNotificationType): bool
+    public function replicate(User $user, BillboardImage $billboardImage): bool
     {
-        return $user->can('replicate_agent::notification::type');
+        return $user->can('replicate_billboard::image');
     }
 
     /**
@@ -103,6 +103,6 @@ class AgentNotificationTypePolicy
      */
     public function reorder(User $user): bool
     {
-        return $user->can('reorder_agent::notification::type');
+        return $user->can('reorder_billboard::image');
     }
 }
