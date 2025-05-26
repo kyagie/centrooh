6571d028b43641013b2252ebbaf3c836eff2a8c1
Arthur Kirabo <kyagieboy99@gmail.com>
2025-05-26 20:13:29 +0300

    Add migration to include media_owner_id in billboards table with foreign key constraint

--- database/migrations/2025_05_26_201253_add_media_owner_id_to_billboards.php
+++ database/migrations/2025_05_26_201253_add_media_owner_id_to_billboards.php
@@ -12,6 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('billboards', function (Blueprint $table) {
+            //
             $table->unsignedBigInteger('media_owner_id')->nullable()->after('agent_id');
             $table->foreign('media_owner_id')->references('id')->on('media_owners')->onDelete('set null');
         });
@@ -23,8 +24,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('billboards', function (Blueprint $table) {
-            $table->dropForeign(['media_owner_id']);
-            $table->dropColumn('media_owner_id');
+            //
         });
     }
 };
