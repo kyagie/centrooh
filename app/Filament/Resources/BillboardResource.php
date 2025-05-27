c995691c7813b330a7d13491d2c2e4c25420d0aa
Arthur Kirabo <kyagieboy99@gmail.com>
2025-05-28 00:46:32 +0300

    Refactor BillboardResource: comment out header actions for future implementation

--- app/Filament/Resources/BillboardResource.php
+++ app/Filament/Resources/BillboardResource.php
@@ -134,10 +134,10 @@ class BillboardResource extends Resource
     public static function table(Table $table): Table
     {
         return $table
-            ->headerActions([
-                ImportAction::make()
-                    ->importer(BillboardImporter::class)
-            ])
+            // ->headerActions([
+            //     ImportAction::make()
+            //         ->importer(BillboardImporter::class)
+            // ])
             ->columns([
                 Tables\Columns\TextColumn::make('name')
                     ->searchable(),
@@ -181,6 +181,7 @@ class BillboardResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                
             ])
             ->bulkActions([]);
     }
